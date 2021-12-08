35abc038166f317cd8e25d00af30bf9a4c4f771b
eds9nsilva <antonioedson99@gmail.com>
2021-12-07 21:15:27 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadePratica/AtividadePratica.cpp
@@ -0,0 +1,20 @@
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    // Declarando meu RU em um vetor. 
+    // Desta forma fica mais simples de trabalhar com as posições dos digitos
+    int meuRuAluno[] = {3,2,9,2,5,9,1}; 
+
+    int tempoLigacao4Digito = meuRuAluno[3]; // Pegando o valor do vetor na 4° posição, o vetor inicia a partir de 0.
+
+    printf("Tempo de ligacao igual ao 4 digito do meu RU: %i \n", tempoLigacao4Digito);
+
+    int tempoLigacaoSoma = meuRuAluno[2] + meuRuAluno[5];
+
+    printf("Tempo de ligacao igual a soma do terceiro com o sexto digito do meu RU: %i", tempoLigacaoSoma);
+
+
+
+}
